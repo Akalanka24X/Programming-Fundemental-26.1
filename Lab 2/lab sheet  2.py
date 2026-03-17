8e00333bc720b7e216622b50ac112d2dffeb4388
Akalanka24X <akalanka88j@gmail.com>
2026-03-17 15:54:51 +0530

    Create lab sheet  2.py

---
+++ Lab 2/lab sheet  2.py
@@ -0,0 +1,80 @@
+#Question 1
+
+print("Akalanka Jayasinghe")
+print("St. Thomas College Matale")
+
+# This is a single-line comment
+
+"""
+This is a multi-line comment
+"""
+
+#comments are used in python to explain the code and make it easier to undestand
+
+
+
+#Question 2
+
+print("22 divided by 7 is:",3.142857142857143)
+
+
+#Question 3
+
+print ("Concatenation means joining strings together")
+
+a="sum"
+b="8"
+result = a + " is " + b
+print ("\n" + result)
+
+
+#Question 4
+
+length = 10
+width = 5
+Area = "The area of a rectangle with length 10 and width 5 is " + str(length * width)
+print("\n" + Area)
+
+#Question 5
+name = input("Enter your name: ")
+
+print("Hello " + name + ", welcome to NSBM!")
+
+print("Data type of name is:", type(name))
+
+#Question 6
+
+birth_year = input("Enter your birth year: ")
+
+birth_year = int(birth_year)
+
+print("Your birth year is:", birth_year)
+
+print("Data type of birth year is:", type(birth_year))
+
+#typecasting means converting one data type into another
+
+#Question 7
+
+birth_year = int(input ("What is your birth year ? "))
+current_year = int(2025)
+age = current_year - birth_year 
+print ("your age is :", age)
+
+
+#Bonus Question
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
+print("Power:", num1 ** num2)
+
+
+
+
+
+
